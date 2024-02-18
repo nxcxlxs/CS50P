9bb698c9e065937c6c5ce972634c85f68a85bffb
nxcxlxs <nick_mro2@hotmail.com>
2024-02-18 19:13:01 -0300

    Problem Set 4

---
+++ pset4/bitcoin.py
@@ -0,0 +1,18 @@
+import sys
+import requests
+import json
+
+response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+
+obj = response.json()
+usd_data = obj["bpi"]["USD"]
+amount = usd_data["rate_float"]
+
+try:
+    if sys.argv[1].isalpha() == True:
+        sys.exit("Command-line argument is not a number")
+    else:
+        print(f"${float(sys.argv[1]) * amount:,.4f}")
+
+except IndexError:
+    sys.exit("Missing command-line argument")
