98c413f2d611ee674fc43d1c7980ea153e9d48d8
nxcxlxs <nick_mro2@hotmail.com>
2024-02-19 18:59:21 -0300

    Problem Set 4

--- pset4/bitcoin.py
+++ pset4/bitcoin.py
@@ -5,6 +5,7 @@ import json
 response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 
 obj = response.json()
+# print(json.dumps(obj, indent=1)) Helps visualize the data while indented.
 usd_data = obj["bpi"]["USD"]
 amount = usd_data["rate_float"]
 
